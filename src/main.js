12d191cefe41da9ebe6e0bfb84150156fc2ae76e
Phil Zona <phil.b.zona@gmail.com>
2023-07-21 10:02:13 -0400

    Add reveal js scaffold

--- src/main.js
+++ src/main.js
@@ -1,48 +1,75 @@
+import Reveal from 'reveal.js';
 import gsap from 'gsap';
 
-const tl = gsap.timeline({ 
+// Slide deck setup
+Reveal.initialize();
+
+const deck = new Reveal();
+deck.initialize();
+
+// Animation
+const tl = gsap.timeline({
   defaults: {
-    duration: .4
+    duration: 0.4,
   },
-  repeat: -1
+  repeat: -1,
 });
 
 const baseFadeout = {
   duration: 1,
   opacity: 0,
   clipPath: 'circle(10% at 50% 50%)',
-  y: -30
+  y: -30,
 };
 
 const spread = 60;
 
-tl
-  .add('start')
+tl.add('start')
   .from('.unit', {
     y: -60,
     opacity: 0,
-    stagger: .4,
-    ease: 'bounce'
+    stagger: 0.4,
+    ease: 'bounce',
   })
-  .from('h1', {
-    y: 60,
-    duration: .6
-  }, 'start')
+  .from(
+    'h1',
+    {
+      y: 60,
+      duration: 0.6,
+    },
+    'start'
+  )
   .add('end')
-  .to('#unit1', {
-    x: spread * -1,
-    ...baseFadeout
-  }, 'end')
-  .to('#unit2', {
-    ...baseFadeout
-  }, 'end')
-  .to('#unit3', {
-    x: spread,
-    ...baseFadeout
-  }, 'end')
-  .to('h1', {
-    duration: 1,
-    opacity: 0,
-    letterSpacing: '40px',
-    y: 20
-  }, 'end');
+  .to(
+    '#unit1',
+    {
+      x: spread * -1,
+      ...baseFadeout,
+    },
+    'end'
+  )
+  .to(
+    '#unit2',
+    {
+      ...baseFadeout,
+    },
+    'end'
+  )
+  .to(
+    '#unit3',
+    {
+      x: spread,
+      ...baseFadeout,
+    },
+    'end'
+  )
+  .to(
+    'h1',
+    {
+      duration: 1,
+      opacity: 0,
+      letterSpacing: '40px',
+      y: 20,
+    },
+    'end'
+  );
